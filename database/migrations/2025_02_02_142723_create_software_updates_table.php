8808866c19fd24cba319408e0165a059fd0e11c5
vhratts <118541950+vratts@users.noreply.github.com>
2025-02-25 13:05:00 -0300

    ✅ Bug correction for updater function #4

--- database/migrations/2025_02_02_142723_create_software_updates_table.php
+++ database/migrations/2025_02_02_142723_create_software_updates_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
         });
 
         SoftwareUpdate::create([
-            'version' => '0.0.2',
+            'version' => '0.0.3',
             'reliase' => 'stable',
             'repository' => 'RattsTechServices/arthemys',
             'artefact' => '#',
